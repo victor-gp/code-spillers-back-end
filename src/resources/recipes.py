bfca90d487afeb6faa48e0fc1e5f28db17f178dc
victor-gp <victor.gp.dev@gmail.com>
2020-10-10 17:59:24 +0200

    Parse JSON input

--- src/resources/recipes.py
+++ src/resources/recipes.py
@@ -10,23 +10,44 @@ from flask_restful.reqparse import Argument
 from repositories import RecipesRepository
 from util import parse_params
 
+import sys
 
 class RecipesResource(Resource):
     """ Verbs relative to the Recipes """
 
 
     @staticmethod
+    @parse_params(
+        Argument("ingredients", location="json", type=str, action='append',
+                 help="The list of ingredients.")
+    )
     @swag_from("../swagger/recipes/POST.yml")
-    def post():
-        """ Return a list of K recipes based on the sent ingredients """
+    def post(ingredients):
+        """ Return a list of 3 recipes based on the sent ingredients """
 
-        ingredients = [
-            'apples', 'bananas', 'mango', 'milk', 'ginger', 'meat',
-            'eggs', 'water', 'caramels', 'juice', 'spinach',
-            'cucumber', 'garlic', 'oil', 'baking soda', 'flour',
-            'potatoes', 'tomatoes', 'onion'
-        ]
+        # debugging
+        print('DEBUG: writing to STDERR works fine', file=sys.stderr)
+        print( ", ".join(ingredients), file=sys.stderr )
+        # takes a workdir reload to show up in the container output (just save!)
 
         recipes = RecipesRepository.get_by_ingredients(ingredients)
 
         return jsonify({"recipes": recipes})
+
+
+"""
+
+expected input:
+
+curl -X POST "http://127.0.0.1:3000/application/recipes/" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"ingredients\" : [ \"apples\", \"bananas\", \"mango\", \"milk\", \"ginger\", \"meat\", \"eggs\", \"water\", \"caramels\", \"juice\", \"spinach\", \"cucumber\", \"garlic\", \"oil\", \"baking soda\", \"flour\", \"potatoes\", \"tomatoes\", \"onion\"]}"
+
+transformed input:
+
+ingredients = [
+    "apples", "bananas", "mango", "milk", "ginger", "meat",
+    "eggs", "water", "caramels", "juice", "spinach",
+    "cucumber", "garlic", "oil", "baking soda", "flour",
+    "potatoes", "tomatoes", "onion"
+]
+
+"""
